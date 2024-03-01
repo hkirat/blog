53983a3e6ce0600a2764c4e7f201e043435714e5
harkirat singh <harkiratsingh@harkirats-MacBook-Pro-2.local>
2024-03-01 18:57:48 +0530

    Type error

--- frontend/src/components/BlogCard.tsx
+++ frontend/src/components/BlogCard.tsx
@@ -45,7 +45,7 @@ export function Circle() {
     </div>
 }
 
-export function Avatar({ name, size = "small" }: { name: string, size: "small" | "big" }) {
+export function Avatar({ name, size = "small" }: { name: string, size?: "small" | "big" }) {
     return <div className={`relative inline-flex items-center justify-center overflow-hidden bg-gray-600 rounded-full ${size === "small" ? "w-6 h-6" : "w-10 h-10"}`}>
     <span className={`${size === "small" ? "text-xs" : "text-md"} font-extralight text-gray-600 dark:text-gray-300`}>
         {name[0]}
--- frontend/src/pages/Blog.tsx
+++ frontend/src/pages/Blog.tsx
@@ -11,7 +11,7 @@ export const Blog = () => {
         id: id || ""
     });
 
-    if (loading) {
+    if (loading || !blog) {
         return <div>
             <Appbar />
         
